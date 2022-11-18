7ef52c7a1c6fe5cce3ecb85913a16a56c0a8936f
Sa888nO <shura.reno.97@bk.ru>
2022-11-19 01:06:37 +0300

    + com-t button for update code style :)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,14 @@
-import styles from "./Button.module.scss";
+import styled from "styled-components";
 
-const Button = () => {
+const StandartButton = styled.button`
+	min-width: 30px;
+	min-height: 30px;
+	font-size: 20px;
+	font-weight: bold;
+`;
 
-}
+const Button = ({ content, ...props }) => {
+	return <StandartButton {...props}>{content}</StandartButton>;
+};
 
-export default Button
+export default Button;
--- src/pages/Calc/Calc.jsx
+++ src/pages/Calc/Calc.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
+import Button from "@components/Button";
 import styles from "./Calc.module.scss";
 
 const Calc = () => {
 	const [expression, updateExpression] = useState("");
+
+	const addElToExp = (element) => {
+		updateExpression(expression + element);
+	};
+
+	const deleteEl = () => {
+		updateExpression(expression.slice(0, -1));
+	};
+	const deleteAllEl = () => {
+		updateExpression("");
+	};
 
 	return (
 		<div className={styles.wrapper}>
@@ -11,152 +23,38 @@ const Calc = () => {
 			<div className={styles.buttonBlock}>
 				<div className={styles.numbersBlock}>
 					<div>
-						<button
-							onClick={() => {
-								updateExpression(expression + "1");
-							}}
-						>
-							1
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "2");
-							}}
-						>
-							2
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "3");
-							}}
-						>
-							3
-						</button>
+						<Button content={1} onClick={() => addElToExp("1")} />
+						<Button content={2} onClick={() => addElToExp("2")} />
+						<Button content={3} onClick={() => addElToExp("3")} />
 					</div>
 					<div>
-						<button
-							onClick={() => {
-								updateExpression(expression + "4");
-							}}
-						>
-							4
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "5");
-							}}
-						>
-							5
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "6");
-							}}
-						>
-							6
-						</button>
+						<Button content={4} onClick={() => addElToExp("4")} />
+						<Button content={5} onClick={() => addElToExp("5")} />
+						<Button content={6} onClick={() => addElToExp("6")} />
 					</div>
 					<div>
-						<button
-							onClick={() => {
-								updateExpression(expression + "7");
-							}}
-						>
-							7
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "8");
-							}}
-						>
-							8
-						</button>
-						<button
-							onClick={() => {
-								updateExpression(expression + "9");
-							}}
-						>
-							9
-						</button>
+						<Button content={7} onClick={() => addElToExp("7")} />
+						<Button content={8} onClick={() => addElToExp("8")} />
+						<Button content={9} onClick={() => addElToExp("9")} />
 					</div>
 					<div>
-						<button
-							onClick={() => {
-								updateExpression(expression + "0");
-							}}
-						>
-							0
-						</button>
+						<Button content={0} onClick={() => addElToExp("0")} />
 					</div>
 				</div>
 				<div>
-					<button
-						onClick={() => {
-							updateExpression(expression + "+");
-						}}
-					>
-						+
-					</button>
-					<button
-						onClick={() => {
-							updateExpression(expression + "-");
-						}}
-					>
-						-
-					</button>
-					<button
-						onClick={() => {
-							updateExpression(expression + "*");
-						}}
-					>
-						*
-					</button>
-					<button
-						onClick={() => {
-							updateExpression(expression + "/");
-						}}
-					>
-						/
-					</button>
-					<button
-						onClick={() => {
-							updateExpression(expression + "^");
-						}}
-					>
-						^
-					</button>
+					<Button content={"+"} onClick={() => addElToExp("+")} />
+					<Button content={"-"} onClick={() => addElToExp("-")} />
+					<Button content={"*"} onClick={() => addElToExp("*")} />
+					<Button content={"/"} onClick={() => addElToExp("/")} />
+					<Button content={"^"} onClick={() => addElToExp("^")} />
 				</div>
 				<div>
-					<button
-						onClick={() => {
-							updateExpression(expression + "(");
-						}}
-					>
-						(
-					</button>
-					<button
-						onClick={() => {
-							updateExpression(expression + ")");
-						}}
-					>
-						)
-					</button>
+					<Button content={"("} onClick={() => addElToExp("(")} />
+					<Button content={")"} onClick={() => addElToExp(")")} />
 				</div>
 				<div className={styles.deleteBlock}>
-					<button
-						onClick={() => {
-							updateExpression(expression.slice(0, -1));
-						}}
-					>
-						Delete
-					</button>
-					<button
-						onClick={() => {
-							updateExpression("");
-						}}
-					>
-						Clear
-					</button>
+					<Button content={"Delete"} onClick={deleteEl} />
+					<Button content={"Clear"} onClick={deleteAllEl} />
 				</div>
 				<button className={styles.getAnswerButton}>=</button>
 			</div>
